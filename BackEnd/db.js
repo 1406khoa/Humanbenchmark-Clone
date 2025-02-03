28dd99a948b56482c94b829430765e435f12ad3f
khoa1406 <khoa.nd11530@sinhvien.hoasen.edu.vn>
2025-02-03 16:12:10 +0700

    debug4

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -2,14 +2,17 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 const { URL } = require('url');
 
-const dbUrl = process.env.MYSQL_URL;
+// Xây dựng DSN từ biến môi trường
+const dbUrl = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_TCP_PROXY_DOMAIN}:${process.env.RAILWAY_TCP_PROXY_PORT}/${process.env.MYSQL_DATABASE}`;
+
+// Kiểm tra xem dbUrl có rỗng không
 if (!dbUrl) {
-  throw new Error("❌ Chưa có biến môi trường MYSQL_URL!");
+  throw new Error("❌ Chưa có chuỗi DSN MySQL (dbUrl)!");
 }
 
 // Tách DSN
 const { hostname, username, password, pathname, port } = new URL(dbUrl);
-const database = pathname.slice(1); // bỏ dấu "/"
+const database = pathname.slice(1); // Bỏ dấu "/"
 
 const pool = mysql.createPool({
   host: hostname,
@@ -19,12 +22,12 @@ const pool = mysql.createPool({
   port
 });
 
-// Kiểm tra kết nối (test)
+// Thử kết nối khi khởi chạy
 (async () => {
   try {
-    const conn = await pool.getConnection(); // Kết nối thử
+    const conn = await pool.getConnection();
     console.log("✅ Kết nối MySQL thành công!");
-    conn.release(); // Trả connection về pool
+    conn.release();
   } catch (err) {
     console.error("❌ Không thể kết nối MySQL:", err);
   }
